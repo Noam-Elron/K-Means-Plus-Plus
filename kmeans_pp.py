ad812c5defbea158efde35aff22fe7e1049b0d92
Noam-Elron <noamelron@mail.tau.ac.il>
2025-01-06 17:31:23 +0000

    Finished (if ignoring small error) Kmeansplusplus algorithm
    Co-authored-by: inbalbarzilay <inbalbarzilay@users.noreply.github.com>

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -2,13 +2,104 @@ import argparse
 import os
 import numpy as np
 import pandas as pd
+import math
+from typing import List, Union
 
-def kmeansplusplus():
-    pass
+def kmeansplusplus(K: int, dataframe: pd.DataFrame) -> List[List[float]]:
+    """
+    Method described in HW2 to initialize the first K centroids
 
+    Parameters
+    ----------
+    K : int
+        Number of centroids to be initialized
+    dataframe : pd.DataFrame
+        Dataframe of all points that we are given (two files were given and were inner joined and sorted (essentially just in each row of the first point coordinates the last
+        n-1 points in the second input file in the corresponding row(they have same keys on the first column)))
 
+    Returns
+    -------
+    List[List[float]]
+        _description_
+    """
+    np.random.seed(1234)
+    centroids = []
+    rows = dataframe.size   
+    initial_centroid_index = np.random.choice(list(range(rows)))
+    initial_centroid = dataframe.iloc[initial_centroid_index]
+    centroids.append(initial_centroid)
+    dataframe.drop([initial_centroid_index], axis=0, inplace=True)
+    
+    while len(centroids) < K:
+        total_dist = 0
+        distances = np.array([])
+        rows = dataframe.size
 
+        for row in dataframe.values:
+            closest_centroid_distance = closest_cluster_distance(centroids, row)
+            distances.append(closest_centroid_distance)
+            total_dist += closest_centroid_distance
+
+        probabilities = calc_probabilities(distances, total_dist)
+        centroid_index = np.random.choice(list(range(rows)), p=probabilities)
+        centroid = dataframe.iloc[centroid_index]
+        centroids.append(centroid)
+        dataframe.drop([centroid_index], axis=0, inplace=True)
+    
+    return centroids
+
+        
+
+def calc_probabilities(distances_list, total_distances):
+    """
+
+    Parameters
+    ----------
+    distances_list : List[List[float]]
+         List of distances of each point from its nearest centroid, index of point based on row in dataframe
+    total_distances : float
+        Sum of distances list
+    
+    Returns
+    ----------
+    List[float]
+        List of probability of each point to get chosen as the next initial centroid.
+    """
+    probabilities = []
+
+    for i in range(len(distances_list)):
+        probabilities.append(distances_list[i]/total_distances)
+    return probabilities
+
+def read_files(filepath1: str, filepath2: str) -> pd.DataFrame:
+    """
+
+    Parameters
+    ----------
+    distances_list : _type_
+        _description_
+    total_distances : _type_
+        _description_
+        
+    """
+    df_file1: pd.DataFrame = pd.read_csv(filepath1, header=None)
+    df_file2: pd.DataFrame = pd.read_csv(filepath2, header=None)
+    inner_join_df: pd.DataFrame = pd.merge(df_file1, df_file2, how='inner', left_on=0, right_on=0)
+    
+    inner_join_df.sort_values(by=0, inplace=True)    
+    return inner_join_df
+    
 def parse() -> argparse.Namespace:
+    """
+
+    Parameters
+    ----------
+    distances_list : _type_
+        _description_
+    total_distances : _type_
+        _description_
+        
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('K', type=str)
     parser.add_argument('iter', type=str, nargs='?', default="300")
@@ -17,6 +108,44 @@ def parse() -> argparse.Namespace:
     parser.add_argument('file_name_2', type=str)
     return parser.parse_args()
 
+def euclidean_distance(point, other) -> float:
+    """
+
+    Parameters
+    ----------
+    distances_list : _type_
+        _description_
+    total_distances : _type_
+        _description_
+        
+    """
+    total = 0
+    for i in range(len(point)):
+        total += pow((point[i] - other[i]), 2)
+    return math.sqrt(total)
+
+def closest_cluster_distance(found_centroids, point) -> int:
+    """
+
+    Parameters
+    ----------
+    found_centroids : List[List[float]]
+        List of all currently initialized centroids
+    point : List[float]
+        Point that we are currently checking the distance between it and its nearest current centroid
+
+    Returns
+    -------
+    int
+        Distance between point and the nearest centroid of those that have been discovered so far.
+    """
+
+    closest_dist = euclidean_distance(point, found_centroids[0])
+    for i in range(1, len(found_centroids)):
+        dist = euclidean_distance(point, found_centroids[i])
+        if dist < closest_dist:
+            closest_dist = dist
+    return closest_dist
 
 
 if __name__ == "__main__":
@@ -31,3 +160,8 @@ if __name__ == "__main__":
 
     except ValueError:
         print("An Error has Occurred")
+
+    points_dataframe = read_files(file_name_1, file_name_2)
+
+    centroids = kmeansplusplus(K, points_dataframe)
+    print(centroids)
